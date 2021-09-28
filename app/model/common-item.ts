5ec8eab71d4fb18e2dfdbb894cbea2d5e8f6d1b8
anpnv <0212anponamarev@gmail.com>
2021-09-28 11:03:29 +0200

    common-item : done

--- app/model/common-item.ts
+++ app/model/common-item.ts
@@ -3,6 +3,12 @@ import { ItemHandler } from "../handler/item-handler";
 
 export class CommonItem extends ItemHandler {
   updateQuality(item: Item): Item {
+    if(item.sellIn <= 0){
+      item.quality = Math.max(item.quality -2, this.MIN_QUALITY);
+    } else {
+      item.quality = Math.max(item.quality -1, this.MIN_QUALITY);
+    }
+    item.sellIn -1;
     return item;
   }
 }
