347ed3b54b6806bb5059d05cbb9db0fe8a8a6fba
anpnv <0212anponamarev@gmail.com>
2021-09-28 11:35:14 +0200

    backstage-passes-item : bug fixed

--- app/model/backstage-passes-item.ts
+++ app/model/backstage-passes-item.ts
@@ -5,11 +5,9 @@ export class BackstagePassesItem extends ItemHandler {
   updateQuality(item: Item): Item {
     if (item.sellIn <= 0) {
       item.quality = 0;
-    }
-    if (item.sellIn <= 5) {
+    } else if (item.sellIn <= 5) {
       item.quality = Math.min(item.quality + 3, this.MAX_QUALITY);
-    }
-    if (item.sellIn <= 10) {
+    } else if (item.sellIn <= 10) {
       item.quality = Math.min(item.quality + 2, this.MAX_QUALITY);
     } else {
       item.quality = Math.min(item.quality + 1, this.MAX_QUALITY);
