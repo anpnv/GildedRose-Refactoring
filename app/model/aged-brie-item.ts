d835e00b000e2bff78530cd0f37118aeadbc40e8
anpnv <0212anponamarev@gmail.com>
2021-09-28 10:55:55 +0200

    Aged-brie-item : done

--- app/model/aged-brie-item.ts
+++ app/model/aged-brie-item.ts
@@ -3,6 +3,13 @@ import { ItemHandler } from "../handler/item-handler";
 
 export class AgedBrieItem extends ItemHandler {
   updateQuality(item: Item): Item {
+    item.sellIn -= 1;    
+    if (item.sellIn <= 0) {
+      item.quality = Math.min(item.quality + 2, this.MAX_QUALITY);
+    } else {
+      item.quality = Math.min(item.quality + 1, this.MAX_QUALITY);
+    }
+    
     return item;
   }
 }
